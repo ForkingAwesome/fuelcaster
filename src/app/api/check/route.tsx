948d6eab487f1c801abb7797439d35f90c052e4b
manav <manav18gadhiya@gmail.com>
2024-04-24 22:24:55 +0530

    contract

--- src/app/api/check/route.tsx
+++ src/app/api/check/route.tsx
@@ -4,9 +4,9 @@ import { abi } from "@/abi";
 
 export async function POST(req: NextRequest, res: NextResponse) {
     const provider = await Provider.create('https://beta-5.fuel.network/graphql');
-    const wallet = Wallet.fromAddress("fuel1efzsz72rw6yhd393l7gmcatt2j46k6y2adu5mn2qh6xr6hd3g6qs8u3f57", provider);
+    const wallet = Wallet.fromPrivateKey(process.env.NEXT_WALLET_PRIVATE_KEY || '', provider);
     const contractId = "0xa080096bc7b7f5d9ff14b98290ac2df65ebf8d96b7116f7eb2f175a4a9be3ea9";
-    
+
     const contract = new Contract(contractId, abi, wallet);
     const value = await contract.functions.increment(1).get();
 
